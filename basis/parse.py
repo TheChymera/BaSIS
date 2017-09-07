6c6ae1b41eb1d9f93179a8c3f9495a760cbd15a9
Horea Christian <horea.christ@yandex.com>
2017-09-08 01:20:49 +0200

    optional saving functionality

--- basis/parse.py
+++ basis/parse.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 from os import path
 
-def bluecover_gps_to_csv(infile,
-	outfile="output.csv",
+def bluecover_gps(infile,
+	save_as="output.csv",
 	):
 	"""Covert gpsapp `.csv` file to `.csv`.
 	"""
@@ -10,16 +10,18 @@ def bluecover_gps_to_csv(infile,
 	infile = path.abspath(path.expanduser(infile))
 
 	with open(infile) as f:
-		x = f.read().replace('\n', '')
-	y=BeautifulSoup(x, "lxml")
+		inlined = f.read().replace('\n', '')
+	soup=BeautifulSoup(inlined, "lxml")
 
-	wpts = y.findAll("wpt")
-	with open(outfile, 'w') as f:
-		for wpt in wpts:
-			try:
-				f.write(wpt.time.text + "," + wpt.extensions.tags.text + "\n")
-			except:
-				f.write(wpt.time.text + ", notag\n")
+	wpts = soup.findAll("wpt")
+
+	if save_as:
+		with open(save_as, 'w') as f:
+			for wpt in wpts:
+				try:
+					f.write(wpt.time.text + "," + wpt.extensions.tags.text + "\n")
+				except:
+					f.write(wpt.time.text + ", notag\n")
 
 if __name__ == "__main__":
-	from_gpsapp('~/Downloads/GPSWaypoints-2017-08-08.gpx')
+	bluecover_gps('~/Downloads/GPSWaypoints-2017-08-08.gpx')
